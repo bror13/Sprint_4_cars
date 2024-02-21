d6940a584157d660f19368f869a4f0abb741bfff
bror13 <159671036+bror13@users.noreply.github.com>
2024-02-20 18:03:11 -0800

    Final commit for built visuals

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Sprint_4_cars
+
+This project is a tool to analyze data on car advertisements with vehicle pricing information. 
--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import streamlit as st
+
 import plotly.express as px
-import altair as at
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 st.header('Analysis of Vehicle Markets')
 st.write('Filter by vehicle make in the drop down below')
@@ -44,15 +46,22 @@ st.dataframe(df_filtered)
 
 #moving on to price analysis
 st.header('Price Analysis')
-st.write('Here is a look at what conditions have the most impact on the price of the vehicle')
+st.write('Here is a look at how different variables impact the price of the vehicle')
 
 list_for_hist = ['condition', 'transmission', 'type']
 
 selected_variable = st.selectbox('Select a variable', list_for_hist)
 
-fig1 = px.histogram(df, x="price", color= selected_variable, range_x=[0, 50000] )
-fig1.update_layout(title= "<b> Visual of price by {}</b>".format(selected_variable))
 
+check = st.checkbox('Exclude cars over $50,000')
+# Conditional to filter the dataframe based on the checkbox.
+if check:
+    df_filtered = df[df['price'] <= 50000]
+else:
+    df_filtered = df
+
+fig1 = px.histogram(df_filtered, x="price", color=selected_variable)
+fig1.update_layout(title="<b> Visual of price by {}</b>".format(selected_variable))
 st.plotly_chart(fig1)
 
 #moving on to scatterplot analysis of different variables
@@ -67,7 +76,9 @@ def age_category(x):
     elif x>=10 and x<20: return '10-20'
     else: return '>20'
 
-#applying the age category function to the age column to create a new column
+
+st.write('Visualizing price with scatterplots from various parameters')
+
 df['age_category'] = df['age'].apply(age_category)
 
 list_for_scatter = ['odometer', 'cylinders', 'days_listed']
@@ -78,3 +89,53 @@ fig2 = px.scatter(df, x= "price", y= choice_for_scatter, color= "age_category", 
 fig2.update_layout(title= "<b> Visual of price by {}</b>".format(choice_for_scatter))
 
 st.plotly_chart(fig2)
+
+#This next histogram will visualize odometer vs. various conditions
+
+st.header('Analysis of Listings by odometer values')
+st.write('Select a variable to compare vehicle mileage')
+
+check = st.checkbox('Exclude cars over 300k miles')
+# Conditional to filter the dataframe based on the checkbox.
+if check:
+    df_filtered = df[df['odometer'] <= 300000]
+else:
+    df_filtered = df
+
+
+list_for_hist2 = ['condition', 'transmission', 'type']
+
+selected_variable2 = st.selectbox('Vehicle Characteristic', list_for_hist2)
+
+fig3 = px.histogram(df_filtered, x="odometer", color= selected_variable2)
+fig3.update_layout(title= "<b> Visual of odometer by {}</b>".format(selected_variable2))
+
+st.plotly_chart(fig3)
+
+
+
+#This visual will compare average price by 'type' and 'make'
+
+st.header('How does the vehicle type and make affect the average list price?')
+
+avg_price_type = df.groupby('type')['price'].mean().reset_index()
+avg_price_make = df.groupby('make')['price'].mean().reset_index() 
+
+data_for_barplot = {
+    "type": avg_price_type, 
+    "make": avg_price_make
+}
+
+list_for_barplot = ['type', 'make']
+
+selected_variable3 = st.selectbox('Vehicle Characteristic', list_for_barplot)
+
+fig4 = px.bar(data_for_barplot[selected_variable3], x="price", color=selected_variable3)
+fig4.update_layout(title= "<b> Visual of average price by {}</b>".format(selected_variable3))
+
+st.plotly_chart(fig4)
+
+
+
+
+
